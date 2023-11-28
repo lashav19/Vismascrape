ad4228841e806c3bc636838374cb25728a70f840
B4Z1C <lashav19@o365.mrvgs.no>
2023-11-28 21:05:59 +0100

    laget ferdig klassen

--- vismalib.py
+++ vismalib.py
@@ -13,29 +13,29 @@ import os
 import dotenv
 
 
-
-
 class Visma:
-    Username: str
-    Password: str
 
-    def __init__(self, *args: str) -> None:
-        
+    Username = ""
+    Password = ""
+
+    def __init__(self, *args: str, debug=False) -> None:
         """
         param --headless:  getting json data
         param start-maximized: Initializing the headless
         Put these as empty to see what is going on for testing purposes  
         """
-
+        self.Username = Visma.Username
+        self.Password = Visma.Password
         self.chrome_driver_path = ChromeDriverManager().install()
         self.service = Service(self.chrome_driver_path)
         self.options = Options()
-        for arg in args:
-            self.options.add_argument(arg)  # Runs Chrome in headless mode.
+        if not debug:
+            for arg in args:
+                self.options.add_argument(arg)
 
+    # Make the program wait for an element to appear to stop crashing
 
-
-    def waitUntil(self, byType: By, item: str): #Make the program wait for an element to appear to stop crashing
+    def waitUntil(self, byType: By, item: str):
 
         wait = WebDriverWait(self.driver, timeout=10)
         wait.until(EC.visibility_of_element_located((byType, item)))
@@ -45,13 +45,14 @@ class Visma:
         print(f"Waited for {item}")
         return waited_for
 
-
     def scrape(self):
-        Username = Visma.Username
-        Password = Visma.Password
 
-        self.driver = webdriver.Chrome(service=self.service, options=self.options)
-        print("started") #Debug
+        self.driver = webdriver.Chrome(
+            service=self.service, options=self.options)
+
+        self.driver.get("https://romsdal-vgs.inschool.visma.no/")
+
+        print("started")  # Debug
 
         # Visit the desired URL
         self.driver.get("https://romsdal-vgs.inschool.visma.no/")
@@ -59,25 +60,26 @@ class Visma:
         # Locate the login button by its name and click it
         time.sleep(.5)
 
-        button = Visma.waitUntil(By.ID, "onetrust-accept-btn-handler")
-        if button: button.click()
+        button = self.waitUntil(By.ID, "onetrust-accept-btn-handler")
+        if button:
+            button.click()
 
-        login = Visma.waitUntil(By.ID, "login-with-feide-button")
+        login = self.waitUntil(By.ID, "login-with-feide-button")
         login.click()
 
         print("logging in")
 
         username = self.driver.find_element(By.ID, "username")
-        username.send_keys(Username)
+        username.send_keys(self.Username)
 
         password = self.driver.find_element(By.ID, "password")
-        password.send_keys(Password)
+        password.send_keys(self.Password)
 
         self.driver.find_element(By.CLASS_NAME, "button-primary").click()
         self.driver.implicitly_wait(2.5)
         print("parsing html")
 
-        Visma.waitUntil(By.CLASS_NAME, "Timetable-TimetableDays_day")
+        self.waitUntil(By.CLASS_NAME, "Timetable-TimetableDays_day")
 
         page_source = self.driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
@@ -93,29 +95,30 @@ class Visma:
                 'div', class_='Timetable-TimetableDays_day', recursive=True)
 
         if parent_div:
-                    
+
             # Find all <h4> elements within the parent <div>
             h4_tags = parent_div.find_all('h4')
 
-            teacher_item = parent_div.find('div', class_="Timetable-Items", recursive=True)
-            teachers = []
+            teacher_item = parent_div.find(
+                'div', class_="Timetable-Items", recursive=True)
 
-            for teacher in teacher_item:
-                items=teacher.find("div", {"teachername": True})
-                teacher_name = items['teachername']
-                teachers.append(teacher_name)
-            
+            teachers = [teacher.find("div", {"teachername": True})[
+                'teachername'] for teacher in teacher_item]
+
             lessons = [h4tag.get_text().split()[0] for h4tag in h4_tags]
 
-            timestart = [h4tag.get_text().split('klokken')[1].split()[0] for h4tag in h4_tags]
-            
+            timestart = [h4tag.get_text().split('klokken')[1].split()[0]
+                         for h4tag in h4_tags]
 
-        dump = {i:[j,k] for i,j,k in zip(timestart, lessons, teachers)}
+        dump = {i: [j, k] for i, j, k in zip(timestart, lessons, teachers)}
         self.driver.close()
         return dump
 
 
 if __name__ == "__main__":
-    visma = Visma()
-    visma.Username = "1234"
-    visma.Password = "13514"
+
+    scraper = Visma("--headless", "start-maximized")
+    scraper.Username = os.getenv("VismaUser")
+    scraper.Password = os.getenv("VismaPassword")
+    dump = scraper.scrape()
+    print(dump)
