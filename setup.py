7abf929911be42d9fd3e549e865947cca7dcb903
B4Z1C <lashav19@o365.mrvgs.no>
2024-04-21 15:20:07 +0200

    chore(update): update version & add long description

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 # Read the contents of requirements.txt
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+
 setup(
     name='python-visma',
-    version='1.0.2',
+    version='1.0.3',
     description='Python API to interact with visma as well as getting credentials to use against the Visma REST API',
+    long_description= long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=requirements,
 )
