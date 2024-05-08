0311f3c23dbb3197bd686a2f3a83a88c7df5589f
lashav19 <lashav19@o365.mrvgs.no>
2024-05-08 09:32:27 +0200

    feat: get auth, XSRF and Learner ID from post request

--- testing/fastlogin.py
+++ testing/fastlogin.py
@@ -1,4 +1,3 @@
-
 import os
 import urllib
 from urllib.parse import unquote, urlparse, parse_qs, urlencode, quote, quote_plus, urljoin
@@ -20,22 +19,18 @@ class visma:
         self.Username = ""
         self.Password = ""
         self.base_url = url if url.endswith('/') else url+"/"
-        self.url = self.base_url + "?idp=feide" 
+        self.url = self.base_url + "Login.jsp/?idp=feide" 
 
     def get_auth(self):
         session = requests.session()
         response = session.get(self.url, allow_redirects=True)
         parsed_url = urllib.parse.urlparse(response.url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        token_input = soup.find('input', {'name': '__RequestVerificationToken'}) # Find the input field with the name '__RequestVerificationToken'
-
-        
-
-        request_verification_token = token_input.get("value") # Get value of requestVerification token
+        token_input = soup.find('input', {'name': '__RequestVerificationToken'}) #* Find the input field with the name '__RequestVerificationToken'
+        request_verification_token = token_input.get("value") #* Get value of requestVerification token
         decoded_url = response.url.replace('%2520', '%20')
         query_params = parse_qs(decoded_url.split('?')[1])
         return_url = query_params.get('ReturnUrl', [''])[0] 
-        print(return_url)
 
         req = session.post("https://connect.visma.com/external/login", 
                             headers={"Cookie": f'.AspNetCore.Antiforgery.D5MU2Fjo4Ro={session.cookies.get(".AspNetCore.Antiforgery.D5MU2Fjo4Ro")};XSRF-TOKEN={session.cookies.get("XSRF-TOKEN")};.AspNetCore.Culture=c%3Den-US%7Cuic%3Den-US'},
@@ -56,16 +51,11 @@ class visma:
         req = session.get(req.headers.get("Location"), allow_redirects=False)
         req = session.get(req.headers.get("Location"), allow_redirects=False)
         req = session.get(req.headers.get("Location"), allow_redirects=False)
-
         data = {
             "feidename": os.getenv("VismaUser"),
             "password": os.getenv("VismaPassword")
         }
-
-
         req = session.post(req.headers.get("Location"), allow_redirects=True, data=data)
-        SAML_AUTHTOKEN = req.headers.get("Set-Cookie")
-
         soup = BeautifulSoup(req.text, 'html.parser')
         SAMLResponse = soup.find('input', {'name': 'SAMLResponse'}).get("value")
         req = session.post("https://auth.dataporten.no/simplesaml/module.php/saml/sp/saml2-acs.php/feide", 
@@ -98,16 +88,11 @@ class visma:
         }
         Cookie = "; ".join([f"{key}={value}" for key, value in data.items()])
 
-
-
         req = session.get(urljoin("https://connect.visma.com", parse_url(req.headers.get("Location"))),
                         headers={"Cookie": Cookie}, 
                         allow_redirects=False)
 
-        req = session.get(urljoin("https://connect.visma.com", parse_url(req.headers.get("Location"))),
-                        headers={"Cookie": req.headers.get("Set-Cookie")}, )
-
-
+        req = session.get(urljoin("https://connect.visma.com", parse_url(req.headers.get("Location"))),headers={"Cookie": req.headers.get("Set-Cookie")}, )
 
         soup = BeautifulSoup(req.text, 'html.parser')
         code = soup.find('input', {'name': 'code'}).get("value")
@@ -124,34 +109,28 @@ class visma:
             "session_state": session_state
         }
 
-
-
         req = session.post("https://app.inschool.visma.no/oauth2/code", data=data, allow_redirects=False)
         req = session.get(req.headers.get("Location"), allow_redirects=False)
         parsed_url = urlparse(req.url)
         root_url = f"{parsed_url.scheme}://{parsed_url.netloc}/"
-        req = session.get(root_url, allow_redirects=True,
-                        data={Cookie: req.headers.get("Set-Cookie")})
+        req = session.get(root_url, allow_redirects=True, data={Cookie: req.headers.get("Set-Cookie")})
 
-        print("XSRF-TOKEN: ", session.cookies.get_dict().get("XSRF-TOKEN"))
-        print("\n AUTHORIZATION: ", session.cookies.get_dict().get("Authorization"))
-
-        learnerID= 'window.index_typeId = 9390648;'
+        print("XSRF-TOKEN: ", session.cookies.get_dict().get("XSRF-TOKEN"), "\n")
+        print("AUTHORIZATION: ", session.cookies.get_dict().get("Authorization"))
 
         pattern = r'window\.(\w+)\s*=\s*(.*?);'
+        match = re.search(pattern, "window.index_typeId = 9390648;")
 
-        match = re.search(pattern, learnerID)
-
-        # If a match is found
         if match:
-            variable_name = match.group(1)  # Extract the variable name
             learnerID = match.group(2)  # Extract the variable value
-
             print(f"Variable Value: {learnerID}")
 
 
-
-
+if __name__ == "__main__":
+    visma = visma("https://romsdal-vgs.inschool.visma.no")
+    visma.Username = os.getenv("VismaUser")
+    visma.Password = os.getenv("VismaPassword")
+    visma.get_auth() 
 
 
 
